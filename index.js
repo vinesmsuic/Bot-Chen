77840e135757e4167873b2ab3fe7b873a8e5a150
Max Ku <34955859+vinesmsuic@users.noreply.github.com>
2018-07-11 14:46:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const botconfig = require("./botconfig.json");
-const tokenfile = require("./tokenfile.json");
+//const botconfig = require("./botconfig.json");
+//const tokenfile = require("./tokenfile.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
