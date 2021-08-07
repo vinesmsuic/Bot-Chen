1eff1b4726457fc20f97af480a42ba36816df665
vinesmsuic <34955859+vinesmsuic@users.noreply.github.com>
2021-08-08 03:01:12 +0800

    Update sound.js

--- commands/sound.js
+++ commands/sound.js
@@ -5,28 +5,38 @@ var files = fs.readdirSync('./sounds/audio_files/');
 
 module.exports.run = async (bot, message, args) => {
     // TO DO CODES
+    if(args[0])
+    {
+        if(args[0] == "help"){
+            msg = "The available sound effects are: \n"
+            files.forEach(file => {
+                msg = msg.concat(file + "\n");
+            });
+            message.reply(msg);
+            return;
+        }
+        else if(files.includes(args[0]))
+        {
+            var voiceChannel = message.member.voice.channel;
 
-    if(args[0] == "help"){
-        msg = "Available sound effects: \n"
-        files.forEach(file => {
-            msg = msg.concat(file + "\n");
-        });
-        message.reply(msg);
-        return;
+            voiceChannel.join().then((connection) => {
+                const dispatcher = connection.play("./sounds/audio_files/".concat(args[0]));
+        
+                dispatcher.on("speaking", (speaking) => {
+                    if (!speaking) {
+                        voiceChannel.leave();
+                    }
+                });
+            })
+        }
+        else
+        {
+            msg = "No file named " + args[0] + ", Please use `.sound help` to check the available sounds."
+            message.reply(msg);
+        }
     }
 
-    var isReady = true;
-    var voiceChannel = message.member.voice.channel;
 
-    voiceChannel.join().then((connection) => {
-        const dispatcher = connection.play("./sounds/audio_files/gg.mp3");
-
-        dispatcher.on("speaking", (speaking) => {
-            if (!speaking) {
-                voiceChannel.leave();
-            }
-        });
-    })
     
 }
 
