8d122b6c422fb8a3e7ae17f816fa67d26ae12c24
vinesmsuic <34955859+vinesmsuic@users.noreply.github.com>
2021-08-08 15:51:57 +0800

    Update sound.js

--- commands/sound.js
+++ commands/sound.js
@@ -22,29 +22,39 @@ module.exports.run = async (bot, message, args) => {
         {
             var voiceChannel = message.member.voice.channel;
 
-            voiceChannel.join().then((connection) => {
-                const dispatcher = connection.play("./sounds/audio_files/".concat(args[0]),{
-                    volume: 0.5,
-                });
-
-                dispatcher.on("speaking", (speaking) => {
-                    isReady = false;
-                    
-                    if (!speaking) {
-                        voiceChannel.leave();
-                        isReady = true;
-                    }
-                });
-            })
+            if(voiceChannel)
+            {
+                voiceChannel.join().then((connection) => {
+                    const dispatcher = connection.play("./sounds/audio_files/".concat(args[0]),{
+                        volume: 0.5,
+                    });
+    
+                    dispatcher.on("speaking", (speaking) => {
+                        isReady = false;
+                        
+                        if (!speaking) {
+                            voiceChannel.leave();
+                            isReady = true;
+                        }
+                    });
+                })
+            }
+            else
+            {
+                message.reply("Join a channel first!");
+                return;
+            }
         }
         else if(isReady == false)
         {
             message.reply("I am busy. Someone is using me to play sounds. Try again later :-)");
+            return;
         }
         else
         {
             msg = "No file named " + args[0] + ", Please use `.sound help` to check the available sounds."
             message.reply(msg);
+            return;
         }
     }
 
