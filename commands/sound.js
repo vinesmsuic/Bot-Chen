1c2e42c3a9bc9b74d8ba9d370ce4aa0fffceba9b
vinesmsuic <34955859+vinesmsuic@users.noreply.github.com>
2021-08-08 15:53:09 +0800

    Update sound.js

--- commands/sound.js
+++ commands/sound.js
@@ -41,7 +41,7 @@ module.exports.run = async (bot, message, args) => {
             }
             else
             {
-                message.reply("Join a channel first!");
+                message.reply("Join a voice channel first!");
                 return;
             }
         }
